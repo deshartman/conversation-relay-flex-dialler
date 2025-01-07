bb910b573782481841b78942eec1aece3ec2fcd6
Des Hartman <des@deshartman.com>
2025-01-08 09:29:23 +1100

    Added try_catch

--- serverless/functions/tools/connect-crelay.js
+++ serverless/functions/tools/connect-crelay.js
@@ -4,27 +4,35 @@
  */
 exports.handler = async function (context, event, callback) {
 
-    // Extract each of the data parameters into "name" and "value", constructing the <Parameter> parts
-    const parameters = `<Parameter name="data" value="${event.data}"/>` // TODO: extract all
+   try {
+      // Extract each of the data parameters into "name" and "value", constructing the <Parameter> parts
+      const parameters = `<Parameter name="data" value="${event.data}"/>` // TODO: extract all
 
-    const callbackTwiml = `<?xml version="1.0" encoding="UTF-8"?>
-    <Response>
-   <Connect >
-      <ConversationRelay 
-         url="wss://${event.serverUrl}" 
-         voice="en-AU-Neural2-A" 
-         dtmfDetection="true" 
-         interruptByDtmf="true" 
-         debug="true">
-            ${parameters}
-    </ConversationRelay>
-   </Connect>
-</Response>`;
+      const callbackTwiml = `<?xml version="1.0" encoding="UTF-8"?>
+       <Response>
+      <Connect >
+         <ConversationRelay 
+            url="wss://${event.serverUrl}" 
+            voice="en-AU-Neural2-A" 
+            dtmfDetection="true" 
+            interruptByDtmf="true" 
+            debug="true">
+               ${parameters}
+         </ConversationRelay>
+      </Connect>
+   </Response>`;
 
-    console.log("twiml ==> ", callbackTwiml);
+      console.log("twiml ==> ", callbackTwiml);
 
-    // Return the twiml to Twilio
-    return callback(null, `${callbackTwiml}`);
+      // Return the twiml to Twilio
+      return callback(null, `${callbackTwiml}`);
+
+
+   } catch (error) {
+      console.error("Error in connect-crelay: ", error);
+      return callback(error);
+
+   }
 
 }
 
