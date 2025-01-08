fabdd6002f3d4389bcba6de3c9093d56b92bf2f9
Des Hartman <des@deshartman.com>
2025-01-08 11:23:40 +1100

    Added Call SID for multi call

--- server/services/ConversationRelayService.js
+++ server/services/ConversationRelayService.js
@@ -13,6 +13,7 @@ class ConversationRelayService extends EventEmitter {
         }
         this.llmService = llmService;
         this.silenceHandler = null;
+        this.callSid = null;
     }
 
     async handleMessage(message) {
@@ -22,28 +23,33 @@ class ConversationRelayService extends EventEmitter {
             if (this.silenceHandler && message.type !== 'info') {
                 this.silenceHandler.resetTimer();
             } else if (message.type === 'info') {
-                console.log("[Conversation Relay] Info message received - Ignoring for timer reset");
+                console.log(`[Conversation Relay with Call SID: ${this.callSid}] Info message received - Ignoring for timer reset`);
+            }
+
+            // For setup messages, set callSid immediately before any logging
+            if (message.type === 'setup') {
+                this.callSid = message.callSid;
             }
 
             // Log the message type
-            console.log(`[Conversation Relay] Received message of type: ${message.type}`);
+            console.log(`[Conversation Relay with Call SID: ${this.callSid}] Received message of type: ${message.type}`);
 
             switch (message.type) {
                 case 'info':
                     break;
                 case 'prompt':
-                    console.info(`[Conversation Relay:] PROMPT >>>>>>: ${message.voicePrompt}`);
+                    console.info(`[Conversation Relay with Call SID: ${this.callSid}] PROMPT >>>>>>: ${message.voicePrompt}`);
                     llmResponse = await this.llmService.generateResponse('user', message.voicePrompt);
-                    console.info(`[Conversation Relay] JSON <<<<<<: ${JSON.stringify(llmResponse, null, 4)}`);
+                    console.info(`[Conversation Relay with Call SID: ${this.callSid}] JSON <<<<<<: ${JSON.stringify(llmResponse, null, 4)}`);
                     return llmResponse;
                 case 'interrupt':
-                    console.info(`[Conversation Relay] INTERRUPT ...... : ${message.utteranceUntilInterrupt}`);
+                    console.info(`[Conversation Relay with Call SID: ${this.callSid}] INTERRUPT ...... : ${message.utteranceUntilInterrupt}`);
                     break;
                 case 'dtmf':
-                    console.debug(`[Conversation Relay] DTMF: ${message.digit}`);
+                    console.debug(`[Conversation Relay with Call SID: ${this.callSid}] DTMF: ${message.digit}`);
                     break;
                 case 'setup':
-                    console.log(`[Conversation Relay] SETUP: Call SID: ${message.callSid} and customer ID: ${message.customParameters.customerReference}`);
+                    console.log(`[Conversation Relay with Call SID: ${this.callSid}] SETUP: Call SID: ${message.callSid} and customer ID: ${message.customParameters.customerReference}`);
 
 
                     // Call the get-customer service passing in the setup message data and getting back the customer data
@@ -67,9 +73,9 @@ class ConversationRelayService extends EventEmitter {
                     // Only generate LLM response if we have greeting text
                     if (customerData.greetingText) {
                         llmResponse = await this.llmService.generateResponse('system', customerData.greetingText);
-                        console.info(`[Conversation Relay] SETUP <<<<<<: ${JSON.stringify(llmResponse, null, 4)}`);
+                        console.info(`[Conversation Relay with Call SID: ${this.callSid}] SETUP <<<<<<: ${JSON.stringify(llmResponse, null, 4)}`);
                     } else {
-                        console.log('[Conversation Relay] No greeting text provided in customer data');
+                        console.log(`[Conversation Relay with Call SID: ${this.callSid}] No greeting text provided in customer data`);
                         llmResponse = {
                             type: "text",
                             token: "Hello, how can I help you today?",
@@ -80,15 +86,21 @@ class ConversationRelayService extends EventEmitter {
                     // Initialize and start silence monitoring. When triggered it will emit a 'silence' event with a message
                     this.silenceHandler = new SilenceHandler();
                     this.silenceHandler.startMonitoring((silenceMessage) => {
+                        // Add callSid to silence message if it's a text message
+                        if (silenceMessage.type === 'text') {
+                            console.log(`[Conversation Relay with Call SID: ${this.callSid}] Sending silence breaker message: ${JSON.stringify(silenceMessage)}`);
+                        } else if (silenceMessage.type === 'end') {
+                            console.log(`[Conversation Relay with Call SID: ${this.callSid}] Ending call due to silence: ${JSON.stringify(silenceMessage)}`);
+                        }
                         this.emit('silence', silenceMessage);
                     });
 
                     return llmResponse;
                 default:
-                    console.log(`[Conversation Relay] Unknown message type: ${message.type}`);
+                    console.log(`[Conversation Relay with Call SID: ${this.callSid}] Unknown message type: ${message.type}`);
             }
         } catch (error) {
-            console.error('[Conversation Relay] Error in message handling:', error);
+            console.error(`[Conversation Relay with Call SID: ${this.callSid}] Error in message handling:`, error);
             throw error;
         }
     }
